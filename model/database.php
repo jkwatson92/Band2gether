3319629049d91509dbe7d8b08d5b22623e538fd4
jkwatson92 <jkwatson@umich.edu>
2018-12-03 13:07:27 -0500

    Started working on displays

---
+++ model/database.php
@@ -0,0 +1,13 @@
+<?php
+    $dsn = 'mysql:host=localhost;dbname=band2gether';
+    $username = 'manager';
+    $password = 'pa55word';
+
+    try {
+        $db = new PDO($dsn, $username, $password);
+    } catch (PDOException $e) {
+        $error_message = $e->getMessage();
+        include('../errors/database_error.php');
+        exit();
+    }
+?>
---
+++ view/header.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+
+<!-- the head section -->
+<head>
+    <title>Band 2gether</title>
+    <link rel="stylesheet" type="text/css"
+          href="/Project4/main.css">
+</head>
+
+<!-- the body section -->
+<body>
+<header>
+    <h1>Band 2gether</h1>
+    <p>Your local music scene solution!</p>
+    <nav>
+        <ul>
+            <li><a href="../">Home</a></li>
+        </ul>
+    </nav>
+</header>
