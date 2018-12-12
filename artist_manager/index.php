e8372b20696013409fb7d5f14cb40d8f2df4fa24
Ashley Baker <ashleybm@umich.edu>
2018-12-11 19:27:03 -0500

    tour selector and removed comments from other files

--- artist_manager/index.php
+++ artist_manager/index.php
@@ -1,4 +1,3 @@
-<!-- Display the technicians  -->
 <?php include '../view/header.php'; ?>
 <?php
 require('../model/database.php');
@@ -25,8 +24,6 @@ $statement->closeCursor();
           <th>Name</th>
           <th>DOB</th>
           <th>Nickname</th>
-          <!-- <th>Email</th>
-          <th>Website</th> -->
           <th>&nbsp;</th>
         </tr>
         <?php foreach($artists as $artist) : ?>
@@ -35,16 +32,9 @@ $statement->closeCursor();
             <td><?php echo $artist['DOB']; ?></td>
             <td><?php echo $artist['Nickname']; ?></td>
             <!-- <td><?php echo $band['Email']; ?></td> -->
-            <!-- Need to add in as hyperlink -->
-            <!-- <td class="right" a href="#"><?php echo $band['Website']; ?></td> -->
-            <!-- <td><form action="delete_tech.php" method="post">
-              <input type="hidden" name="tech_id" value="<?php echo $tech['techID']; ?>">
-              <input type="submit" value="Delete">
-            </form></td> -->
           </tr>
         <?php endforeach; ?>
       </table>
-      <!-- <p><a href="add_tech_form.php">Add Technician</a></p> -->
     </section>
   </main>
 </body>
---
+++ tour_manager/index.php
@@ -0,0 +1,43 @@
+<?php include '../view/header.php'; ?>
+<?php
+require('../model/database.php');
+
+//Get all Tours
+$allTours = 'SELECT * FROM TOUR ORDER BY ';
+$statement = $db->prepare($allTours);
+$statement->execute();
+$tours=$statement->fetchAll();
+$statement->closeCursor();
+?>
+
+<!DOCTYPE html>
+<html>
+<!-- the body section -->
+<body>
+  <main>
+      <!-- display a list of artists -->
+      <h2>Tour List</h2>
+    <section>
+      <!-- display a table of artists -->
+      <table>
+        <tr>
+          <th>Tour Name</th>
+          <th>Headliner</th>
+          <th>&nbsp;</th>
+        </tr>
+        <?php foreach($tours as $tour) : ?>
+          <tr>
+            <td><?php echo $tour['Name']; ?></td>
+            <td><?php echo $tour['Headliner']; ?></td>
+            <td><form action="display_tour.php" method="post">
+              <input type="hidden" name="tour_id" value="<?php echo $tour['tour_id']; ?>">
+              <input name="submit" type="submit" value="Select">
+            </form></td>
+          </tr>
+        <?php endforeach; ?>
+      </table>
+    </section>
+  </main>
+</body>
+</html>
+<!-- <?php include '../view/footer.php'; ?> -->
